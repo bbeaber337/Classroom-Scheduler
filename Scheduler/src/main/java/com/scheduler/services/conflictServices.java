b5d4dbd9260c9c8d95bbd1148baf5dfa636f3952
dmeissner <dmeissner@unomaha.edu>
2016-04-27 18:28:15 -0500

    Added JavaDoc for conflictService class

--- Scheduler/src/main/java/com/scheduler/services/conflictServices.java
+++ Scheduler/src/main/java/com/scheduler/services/conflictServices.java
@@ -15,19 +15,36 @@ public class conflictServices {
 	private validateClassroom validatorClassroom;
 	private validateTeacher validatorTeacher;
 	
+	/**
+	 * Constructor, creates instances of the validation classes for later usage
+	 */
 	public conflictServices() {
 		validatorClassroom = new validateClassroom();
 		validatorTeacher = new validateTeacher();
 	}
 	
+	/**
+	 * @param semester - The semester to check conflicts for
+	 * @return - Returns a list of all conflicts for all class for the specified semester 
+	 */
 	public List<Conflict> getConflicts( String semester) {
 		return getConflicts( semester, (List)null );
 	}
 	
+	/**
+	 * @param semester - The semester to check conflicts for
+	 * @param class1 - The modified class, will check if there are any conflicts between other classes and it
+	 * @return - Returns a list of all conflicts for all class for the specified semester 
+	 */
 	public List<Conflict> getConflicts( String semester, Class1 class1 ) {
 		return getConflicts( semester, Arrays.asList(class1) );
 	}
 	
+	/**
+	 * @param semester - The semester to check conflicts for
+	 * @param classes - The modified class(es), will check if there are any conflicts between other classes and those provided
+	 * @return - Returns a list of all conflicts for all class for the specified semester 
+	 */
 	public List<Conflict> getConflicts( String semester, List<Class1> classes ) {
 		
 		Connection jdbcConn = null;
